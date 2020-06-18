6c7a8a65d544a95cd2fac40e77abb416bd71c49e
liza p <liyourlp@gmail.com>
2020-06-17 19:12:34 -0700

    add generatepassword function

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Declared function generatePassword
+function generatePassword() {
+  //Created var passLength, created a prompt and converted prompt answear to number
+   var passLength = parseInt(prompt("Choose pasword length from 8 - 128 characters"));
+   var password = "";
+   var characters = "abcdefghjklmnopqrstuvwxyz";
+
+    for (var i = 0; i < passLength; i++) {
+      var index = Math.floor(Math.random()*characters.length);
+       password += characters[index];
+
+    }
+    return password;
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword()
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
