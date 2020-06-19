7bcb9c4e5d82f4acccd6bd1a39773d0d18102d52
liza p <liyourlp@gmail.com>
2020-06-18 17:20:45 -0700

    added pass length verification

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 // Declared function generatePassword
 function generatePassword() {
-  //Created var passLength, created a prompt and converted prompt answear to number
+  //Created var passLength, created a prompt and converted prompt answear to number using parseInt
    var passLength = parseInt(prompt("Choose pasword length from 8 - 128 characters"));
+   if(passLength <8 || passLength >128) {
+      alert("Password length should be beetween 8 and 128 characters!");
+      return "";
+   }
+   //Created variable password for generated pass
    var password = "";
    var characters = "abcdefghjklmnopqrstuvwxyz";
 
     for (var i = 0; i < passLength; i++) {
       var index = Math.floor(Math.random()*characters.length);
        password += characters[index];
-
     }
     return password;
 }
